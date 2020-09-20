2e10e7f6c6ff156a4e0ff2138a56866b70975d2f
vsreyes <virgilio.s.reyes@gmail.com>
2020-09-20 23:01:12 +0200

    Added Popup

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -16,7 +16,7 @@ const Popup = ({
     finalMessage = 'Congratulations! You won!🏆';
     playable = false;
   } else if (checkWin(correctLetters, wrongLetters, selectedWord) === 'lose') {
-    finalMessage = 'Unfortunately you lost.😭';
+    finalMessage = 'Unfortunately you lost😭';
     finalMessageRevealWord = `...the word was: ${selectedWord}`;
     playable = false;
   }
